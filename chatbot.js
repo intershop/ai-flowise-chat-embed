d93f84a52180715fd5ba75ad2b2df04162de503f
Ami Ouedraogo <A.Ouedraogo@intershop.de>
2026-03-13 11:45:50 +0100

    fix: update apiHost URL by removing trailing slash

--- chatbot.js
+++ chatbot.js
@@ -1,7 +1,7 @@
 import Chatbot from "https://cdn.jsdelivr.net/gh/intershop/ai-flowise-chat-embed@website/intershop/dist/web.js"
     Chatbot.init({
         chatflowid: "574ef77d-cedb-4242-a96f-387e2dd06013",
-        apiHost: "https://ish-flowise-enterprise.cloud.intershop.com/",
+        apiHost: "https://ish-flowise-enterprise.cloud.intershop.com",
         chatflowConfig: {
         // topK: 2
         },
